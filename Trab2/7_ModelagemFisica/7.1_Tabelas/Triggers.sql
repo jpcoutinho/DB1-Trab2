ae085629a73ec04b4eaba25a59fa91745450d7cb
psyny <psyny@hotmail.com>
2016-06-12 20:58:55 -0300

    Update Triggers.sql

--- Trab2/7_ModelagemFisica/7.1_Tabelas/Triggers.sql
+++ Trab2/7_ModelagemFisica/7.1_Tabelas/Triggers.sql
@@ -1,6 +1,7 @@
 -- Functions And Triggers
 SET SCHEMA 'trab2teste';
 
+-- Valor minimo para transação de compra e venda
 DROP FUNCTION transacao_minima() CASCADE;
 CREATE FUNCTION transacao_minima()
 	RETURNS trigger AS
@@ -19,7 +20,12 @@ CREATE FUNCTION transacao_minima()
 	
 CREATE TRIGGER checa_transacao_minima
 	BEFORE INSERT OR UPDATE
-		ON TB_Comprou , TB_Vendeu
+		ON TB_Comprou TB_Comprou
 			FOR EACH ROW
 				EXECUTE PROCEDURE transacao_minima()
 		
+CREATE TRIGGER checa_transacao_minima
+	BEFORE INSERT OR UPDATE
+		ON TB_Comprou TB_Vendeu
+			FOR EACH ROW
+				EXECUTE PROCEDURE transacao_minima()
