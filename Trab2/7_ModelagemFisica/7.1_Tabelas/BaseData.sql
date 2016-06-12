9aee9b26472e1a4bcfca3d9747c501fb1ff142e1
psyny <psyny@hotmail.com>
2016-06-12 17:13:32 -0300

    Create BaseData.sql

---
+++ Trab2/7_ModelagemFisica/7.1_Tabelas/BaseData.sql
@@ -0,0 +1,24 @@
+-- Apenas dados, sem registro de ações
+
+SET SCHEMA 'trab2teste';
+
+-- Pessoas
+
+INSERT INTO TB_Pessoa VALUES ('100000000' , 'Vito Corleone' , 'RG' , 'M' , '19720421' );
+INSERT INTO TB_Pessoa VALUES ('100000001' , 'Caesar Enrico' , 'RG' , 'M' , '19310812' );
+INSERT INTO TB_Pessoa VALUES ('100000002' , 'Johnny Strabler' , 'RG' , 'M' , '19530110' );
+INSERT INTO TB_Pessoa VALUES ('100000003' , 'Joe Krozac' , 'RG' , 'M' , '19370323' );
+INSERT INTO TB_Pessoa VALUES ('100000004' , 'Tony Montana' , 'RG' , 'M' , '19831121' );
+
+INSERT INTO TB_Pessoa VALUES ('90000001' , 'Al Capone' , 'PP' , 'M' , '19871217' );
+INSERT INTO TB_Pessoa VALUES ('90000002' , 'Tommy DeVito' , 'PP' , 'M' , '19901229' );
+INSERT INTO TB_Pessoa VALUES ('90000003' , 'Clemenza' , 'PP' , 'M' , '19930817' );
+INSERT INTO TB_Pessoa VALUES ('90000004' , 'Baby Face Nelson' , 'PP' , 'M' , '19980909' );
+INSERT INTO TB_Pessoa VALUES ('90000005' , 'Rocky Sullivan' , 'PP' , 'M' , '19980912' );
+
+INSERT INTO TB_Pessoa VALUES ('100000005' , 'Bonnie Parker' , 'RG' , 'F' , '19951021' );
+INSERT INTO TB_Pessoa VALUES ('100000006' , 'Elvira Hancock' , 'RG' , 'F' , '19920710' );
+INSERT INTO TB_Pessoa VALUES ('100000007' , 'Mia Wallace' , 'RG' , 'F' , '19960617' );
+INSERT INTO TB_Pessoa VALUES ('100000008' , 'Victoria' , 'RG' , 'F' , '19940307' );
+INSERT INTO TB_Pessoa VALUES ('100000009' , 'Grace Faraday' , 'RG' , 'F' , '19751127' );
+
