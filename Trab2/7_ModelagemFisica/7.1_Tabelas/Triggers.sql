3196de5d54420945d48084c19c834548488202bb
psyny <psyny@hotmail.com>
2016-06-12 21:20:56 -0300

    Update Triggers.sql

--- Trab2/7_ModelagemFisica/7.1_Tabelas/Triggers.sql
+++ Trab2/7_ModelagemFisica/7.1_Tabelas/Triggers.sql
@@ -20,12 +20,45 @@ CREATE FUNCTION transacao_minima()
 	
 CREATE TRIGGER checa_transacao_minima
 	BEFORE INSERT OR UPDATE
-		ON TB_Comprou TB_Comprou
+		ON TB_Comprou
 			FOR EACH ROW
-				EXECUTE PROCEDURE transacao_minima()
+				EXECUTE PROCEDURE transacao_minima();
 		
 CREATE TRIGGER checa_transacao_minima
 	BEFORE INSERT OR UPDATE
-		ON TB_Comprou TB_Vendeu
+		ON TB_Vendeu
 			FOR EACH ROW
-				EXECUTE PROCEDURE transacao_minima()
+				EXECUTE PROCEDURE transacao_minima();
+	
+	
+-- Checa tipo do documento: SSN vs PP
+DROP FUNCTION tipo_documento() CASCADE;
+CREATE FUNCTION tipo_documento()
+	RETURNS trigger AS
+		$BODY$
+			DECLARE
+			BEGIN
+				IF NEW.tipoDoc = 'RG' then
+					IF char_length( NEW.doc ) != 9 THEN
+						RAISE EXCEPTION 'Transação não efetuada'
+						      USING HINT = 'SSN precisa ter 9 digitos';	
+					END IF;
+				ELSE
+					IF char_length( NEW.doc ) != 8 THEN
+						RAISE EXCEPTION 'Transação não efetuada'
+						      USING HINT = 'Numero de passaporte precisa ter 8 digitos';	
+					END IF;
+				END IF;
+				
+				RETURN NEW;
+			END;
+		$BODY$
+	LANGUAGE plpgsql;
+	
+CREATE TRIGGER checa_transacao_minima
+	BEFORE INSERT OR UPDATE
+		ON TB_Pessoa
+			FOR EACH ROW
+				EXECUTE PROCEDURE tipo_documento();			
+				
+	
