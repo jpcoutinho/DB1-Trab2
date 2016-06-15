61fb1dba48e0cadff0a6615ac686235688b8bcaa
psyny <psyny@hotmail.com>
2016-06-15 17:31:33 -0300

    Create ConsultasRascunho.sql

---
+++ Trab2/7.3_GatilhosFuncoes/ConsultasRascunho.sql
@@ -0,0 +1,10 @@
+SET SCHEMA 'trab2teste';
+
+-- Total gasto e ganho pelos clientes em jogos em grupo
+SELECT 
+	CPU.doc_CLI AS Documento_Cliente ,
+	sum(CPU.apostou) AS Total_Apostado ,
+	sum(CPU.ganhou) AS Total_Ganho
+FROM TB_Competiu CPU
+GROUP BY CPU.doc_CLI
+ORDER BY CPU.doc_CLI
