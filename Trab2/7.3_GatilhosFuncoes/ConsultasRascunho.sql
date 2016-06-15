9e1cae74f5750ac5132bea1cc8c57b3ab6ac7428
psyny <psyny@hotmail.com>
2016-06-15 17:39:31 -0300

    Update ConsultasRascunho.sql

--- Trab2/7.3_GatilhosFuncoes/ConsultasRascunho.sql
+++ Trab2/7.3_GatilhosFuncoes/ConsultasRascunho.sql
@@ -4,7 +4,19 @@ SET SCHEMA 'trab2teste';
 SELECT 
 	CPU.doc_CLI AS Documento_Cliente ,
 	sum(CPU.apostou) AS Total_Apostado ,
-	sum(CPU.ganhou) AS Total_Ganho
+	sum(CPU.ganhou) AS Total_Ganho ,
+	sum(CPU.ganhou - CPU.apostou) AS Saldo
 FROM TB_Competiu CPU
 GROUP BY CPU.doc_CLI
-ORDER BY CPU.doc_CLI;
+ORDER BY Saldo DESC;
+
+-- Igual a anterior, porem exibindo os nomes ao inves do numero de documento
+SELECT 
+	PES.nome AS Cliente ,
+	sum(CPU.apostou) AS Total_Apostado ,
+	sum(CPU.ganhou) AS Total_Ganho ,
+	sum(CPU.ganhou - CPU.apostou) AS Saldo
+FROM TB_Competiu CPU , TB_Pessoa PES
+WHERE CPU.doc_CLI = PES.doc
+GROUP BY PES.nome
+ORDER BY Saldo DESC;
