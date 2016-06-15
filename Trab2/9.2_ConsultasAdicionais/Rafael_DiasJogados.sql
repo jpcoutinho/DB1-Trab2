b371379e9519a9986225e38a7b705daf8cb47894
psyny <psyny@hotmail.com>
2016-06-15 18:28:40 -0300

    Update Rafael_DiasJogados.sql

--- Trab2/9.2_ConsultasAdicionais/Rafael_DiasJogados.sql
+++ Trab2/9.2_ConsultasAdicionais/Rafael_DiasJogados.sql
@@ -19,3 +19,21 @@ SELECT
 FROM TB_Jogada JOG , TB_Pessoa PES
 WHERE JOG.doc_CLI = PES.doc
 ORDER BY PES.doc;
+
+-- PESSOAL
+-- Documento do Cliente , Nome do Cliente , Data da Partida EM GRUPO
+SELECT 
+	CPU.doc_CLI AS Documento ,
+	to_char( PAR.data , 'YYYYMMDD' ) AS Data_PartidaEmGrupo
+FROM TB_Partida PAR , TB_Competiu CPU 
+WHERE CPU.doc_CLI = CPU.doc_CLI
+AND PAR.ID = CPU.ID_PAR
+ORDER BY CPU.doc_CLI;
+
+-- Documento do Cliente , Nome do Cliente , Data da Partida INDIVIDUAL
+SELECT 
+	CPU.doc_CLI AS Documento ,
+	to_char( JOG.data , 'YYYYMMDD' ) AS Data_PartidaIndividual
+FROM TB_Jogada JOG
+WHERE JOG.doc_CLI = CPU.doc_CLI
+ORDER BY CPU.doc_CLI;
