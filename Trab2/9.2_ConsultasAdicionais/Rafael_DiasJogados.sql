ff73e94bf2ae844ef2d5dd10e5b2b18cbbc096f4
psyny <psyny@hotmail.com>
2016-06-15 18:38:34 -0300

    Update Rafael_DiasJogados.sql

--- Trab2/9.2_ConsultasAdicionais/Rafael_DiasJogados.sql
+++ Trab2/9.2_ConsultasAdicionais/Rafael_DiasJogados.sql
@@ -20,20 +20,24 @@ FROM TB_Jogada JOG , TB_Pessoa PES
 WHERE JOG.doc_CLI = PES.doc
 ORDER BY PES.doc;
 
--- PESSOAL
+-- PESSOAL - APENAS PARA AJUDAR A CRIAR AS OUTRAS CONEXOES
 -- Documento do Cliente , Nome do Cliente , Data da Partida EM GRUPO
 SELECT 
 	CPU.doc_CLI AS Documento ,
+	MES.tin_FRA AS Franquia ,
 	to_char( PAR.data , 'YYYYMMDD' ) AS Data_PartidaEmGrupo
-FROM TB_Partida PAR , TB_Competiu CPU 
+FROM TB_Partida PAR , TB_Mesa MES , TB_Competiu CPU 
 WHERE CPU.doc_CLI = CPU.doc_CLI
 AND PAR.ID = CPU.ID_PAR
+AND MES.numero = PAR.numero_MES
 ORDER BY CPU.doc_CLI;
 
 -- Documento do Cliente , Nome do Cliente , Data da Partida INDIVIDUAL
 SELECT 
-	CPU.doc_CLI AS Documento ,
+	JOG.doc_CLI AS Documento ,
+	MAQ.tin_FRA AS Franquia , 
 	to_char( JOG.data , 'YYYYMMDD' ) AS Data_PartidaIndividual
-FROM TB_Jogada JOG
-WHERE JOG.doc_CLI = CPU.doc_CLI
-ORDER BY CPU.doc_CLI;
+FROM TB_Jogada JOG , TB_Maquina MAQ
+WHERE JOG.doc_CLI = JOG.doc_CLI
+AND JOG.snumber_MAQ = MAQ.snumber
+ORDER BY JOG.doc_CLI;
