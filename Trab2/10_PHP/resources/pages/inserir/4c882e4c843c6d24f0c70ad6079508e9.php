e264211e3af243395856666ea3c312c6698a4651
theghostcitizen <theomendes@aluno.puc-rio.br>
2016-06-14 21:15:27 -0300

    Add novos arquivos de INSERT PHP

--- Trab2/10_PHP/resources/pages/inserir/4c882e4c843c6d24f0c70ad6079508e9.php
+++ Trab2/10_PHP/resources/pages/inserir/4c882e4c843c6d24f0c70ad6079508e9.php
@@ -3,28 +3,28 @@ require_once('resources/config.php');
 $pdo = BancodeDados::conecta();
 
 if ( !empty($_POST)) {
-        $tin = $_POST['vTIN'];
+        $doc = $_POST['vDOC'];
         $contato = $_POST['vCONTATO'];
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = 'INSERT INTO '. $BDSchema .'TB_ContatoFranquia (tin_FRA,contato) VALUES(?, ?)';
+        $sql = 'INSERT INTO '. $BDSchema .'TB_ContatoPessoa (doc_PES,contato) VALUES(?, ?)';
         $q = $pdo->prepare($sql);
-        $q->execute(array($tin,$contato));
-        header("Location: tabela.php?ntb=0a258ee607f3546bd8d9050ef6697142&tbo=tin_FRA");
+        $q->execute(array($doc,$contato));
+        header("Location: tabela.php?ntb=4c882e4c843c6d24f0c70ad6079508e9&tbo=doc_PES");
       }
 ?>
-<form action="page.php?ntb=0a258ee607f3546bd8d9050ef6697142&tb=1" method="post" data-abide novalidate>
+<form action="page.php?ntb=4c882e4c843c6d24f0c70ad6079508e9&tb=1" method="post" data-abide novalidate>
   <div data-abide-error class="alert callout" style="display: none;">
     <p><i class="fi-alert"></i> Seu formulário possue alguns erros.</p>
   </div>
   <div class="row">
     <div class="medium-6 columns">
-      <label>*Selecione a franquia
-        <select id="select" name="vTIN" required>
-          <option value="">--SELECIONE A FRANQUIA--</option>
+      <label>*Selecione o cliente
+        <select id="select" name="vDOC" required>
+          <option value="">--SELECIONE O CLIENTE--</option>
           <?php
-            $sql = 'SELECT * FROM '. $BDSchema .'TB_Franquia ORDER BY nome ASC';
+            $sql = 'SELECT * FROM '. $BDSchema .'TB_Pessoa ORDER BY nome ASC';
             foreach ($pdo->query($sql) as $row) {
-              echo '<option value="'. $row['tin'] . '">'. $row['nome'] . '</option>';
+              echo '<option value="'. $row['doc'] . '">'. $row['nome'] . '</option>';
             }
           ?>
         </select>
