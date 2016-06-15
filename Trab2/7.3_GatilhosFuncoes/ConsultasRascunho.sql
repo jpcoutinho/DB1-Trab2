a4e137bf3731424e808082b02bf3ad62380ffca6
psyny <psyny@hotmail.com>
2016-06-15 17:42:40 -0300

    Update ConsultasRascunho.sql

--- Trab2/7.3_GatilhosFuncoes/ConsultasRascunho.sql
+++ Trab2/7.3_GatilhosFuncoes/ConsultasRascunho.sql
@@ -1,6 +1,6 @@
 SET SCHEMA 'trab2teste';
 
--- Total gasto e ganho pelos clientes em jogos em grupo
+-- Documento do Cliente, total apostado, total ganho, saldo
 SELECT 
 	CPU.doc_CLI AS Documento_Cliente ,
 	sum(CPU.apostou) AS Total_Apostado ,
@@ -10,7 +10,7 @@ FROM TB_Competiu CPU
 GROUP BY CPU.doc_CLI
 ORDER BY Saldo DESC;
 
--- Igual a anterior, porem exibindo os nomes ao inves do numero de documento
+-- Nome do Cliente, total apostado, total ganho, saldo
 SELECT 
 	PES.nome AS Cliente ,
 	sum(CPU.apostou) AS Total_Apostado ,
@@ -20,3 +20,15 @@ FROM TB_Competiu CPU , TB_Pessoa PES
 WHERE CPU.doc_CLI = PES.doc
 GROUP BY PES.nome
 ORDER BY Saldo DESC;
+
+-- Nome do Cliente, total apostado, total ganho, saldo ; PARA CLIENTES QUE SO PERDERAM DINHEIRO
+SELECT 
+	PES.nome AS Cliente ,
+	sum(CPU.apostou) AS Total_Apostado ,
+	sum(CPU.ganhou) AS Total_Ganho ,
+	sum(CPU.ganhou - CPU.apostou) AS Saldo
+FROM TB_Competiu CPU , TB_Pessoa PES
+WHERE CPU.doc_CLI = PES.doc
+GROUP BY PES.nome
+HAVING sum(CPU.ganhou - CPU.apostou) < 0
+ORDER BY Saldo;
