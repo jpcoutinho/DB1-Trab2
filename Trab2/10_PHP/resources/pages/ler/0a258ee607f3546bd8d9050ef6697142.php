875b432ae91e72df0a53d25831cc666dcedd92d7
theghostcitizen <ghostcitizen@protonmail.ch>
2016-06-17 11:01:25 -0300

    Fix

--- Trab2/10_PHP/resources/pages/ler/0a258ee607f3546bd8d9050ef6697142.php
+++ Trab2/10_PHP/resources/pages/ler/0a258ee607f3546bd8d9050ef6697142.php
@@ -3,12 +3,7 @@ require_once('resources/config.php');
 $pdo = BancodeDados::conecta();
 $tin_fra = $_GET['a'];
 $contato = $_GET['b'];
-
-$sql = "SELECT * FROM '. $BDSchema .'TB_Franquia FRA, '. $BDSchema .'TB_ContatoFranquia COF WHERE FRA.tin = ? && COF.contato = ?";
-$q = $pdo->prepare($sql);
-$q->execute(array($tin_fra,$contato));
-$data = $q->fetch(PDO::FETCH_ASSOC);
-BancodeDados::desconecta();
+$stmt = $pdo->query("SELECT * FROM ". $BDSchema ."TB_Franquia FRA, ". $BDSchema ."TB_ContatoFranquia COF WHERE FRA.tin='". $tin_fra ."' AND COF.contato = '". $contato ."'");
 ?>
 <div class="row">
   <span class="TitTabel">Visualização de um contato de uma franquia</span>
@@ -34,10 +29,14 @@ BancodeDados::desconecta();
         </div>
         <div class="column small-6">
           <?php
-          echo 'Nome: '. $data['FRA.nome'] .' & '. $data['COF.contato'] .' ';
+          $row = $stmt->fetch(PDO::FETCH_ASSOC);
+          echo 'Nome: '. $row['nome'] .' & '. $row['contato'] .' ';
           ?>
         </div>
       </div>
     </div>
   </div>
 </div>
+<?php
+BancodeDados::desconecta();
+?>
