715fc2104b8474463eb08691aebb507ab97116f4
theghostcitizen <theomendes@aluno.puc-rio.br>
2016-06-18 18:15:14 -0300

    Fix

--- Trab2/10_PHP/resources/template/nav.php
+++ Trab2/10_PHP/resources/template/nav.php
@@ -6,7 +6,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="index.html">Casino INF1336</a>
+        <a class="navbar-brand" href="index.php">Casino INF1336</a>
     </div>
     <!-- /.navbar-header -->
     <ul class="nav navbar-top-links navbar-right">
@@ -23,33 +23,34 @@
     <div class="navbar-default sidebar" role="navigation">
         <div class="sidebar-nav navbar-collapse">
             <ul class="nav" id="side-menu">
-                <li><a class="active" href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a></li>
+                <li><a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a></li>
                 <li>
-                    <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tabelas<span class="fa arrow"></span></a>
+                    <a href="#"><i class="fa fa-table fa-fw"></i> Tabelas<span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
-                        <li><a href="#">Caixa</a></li>
-                        <li><a href="#">Cliente</a></li>
-                        <li><a href="#">Competiu</a></li>
-                        <li><a href="#">Comprou</a></li>
-                        <li><a href="#">Contato Franquia</a></li>
-                        <li><a href="#">Franquia</a></li>
-                        <li><a href="#">Frequentou</a></li>
-                        <li><a href="#">Funcionário</a></li>
-                        <li><a href="#">Ganhou</a></li>
-                        <li><a href="#">Jogada</a></li>
-                        <li><a href="#">Jogo</a></li>
-                        <li><a href="#">Jogo em grupo</a></li>
-                        <li><a href="#">Jogo individual</a></li>
-                        <li><a href="#">Máquina</a></li>
-                        <li><a href="#">Mesa</a></li>
-                        <li><a href="#">Oferece</a></li>
-                        <li><a href="#">Partida</a></li>
-                        <li><a href="#">Pessoa</a></li>
-                        <li><a href="#">Vendeu</a></li>
+                        <li><a href="tabela.php?ntb=cbeb4c01b2c581b42decd055eec9076f">Caixa</a></li>
+                        <li><a href="tabela.php?ntb=36b5b40331e8df3f92b5ccf8685afbf5">Cliente</a></li>
+                        <li><a href="tabela.php?ntb=5316b816def2ba41e0241f54dc7efbd1">Competiu</a></li>
+                        <li><a href="tabela.php?ntb=d184455c8eb6fa3bfb9e1440aa465620">Comprou</a></li>
+                        <li><a href="tabela.php?ntb=0a258ee607f3546bd8d9050ef6697142">Contato Franquia</a></li>
+                        <li><a href="tabela.php?ntb=4c882e4c843c6d24f0c70ad6079508e9">Contato Pessoa</a></li>
+                        <li><a href="tabela.php?ntb=607eb7714ecba6f640dd0c1fafb2f42c">Franquia</a></li>
+                        <li><a href="tabela.php?ntb=3abcfcb700acd4310c5c4d9375ad88cf">Frequentou</a></li>
+                        <li><a href="tabela.php?ntb=dbe3d143277fe8475c2e3e88ecf021f5">Funcionário</a></li>
+                        <li><a href="tabela.php?ntb=d9e35a12173a53725317fcabc563c48f">Ganhou</a></li>
+                        <li><a href="tabela.php?ntb=4ba5a8613a22338b62a8fc77deeabf20">Jogada</a></li>
+                        <li><a href="tabela.php?ntb=bceca77ab8c59eeaf2e6d6a323be0ae7">Jogo</a></li>
+                        <li><a href="tabela.php?ntb=0f46c1d95833f44bf262dbfd167edd46">Jogo em Grupo</a></li>
+                        <li><a href="tabela.php?ntb=b6e5a04e7a1d70c4d3d9b3756973a4b5">Jogo Individual</a></li>
+                        <li><a href="tabela.php?ntb=eaff6c1f31f280f1bd68c5d54469f61b">Máquina</a></li>
+                        <li><a href="tabela.php?ntb=16dc2b0d9986b0eb4ad8adfe439496e5">Mesa</a></li>
+                        <li><a href="tabela.php?ntb=42533cc176fe5f805356868aee547210">Oferece</a></li>
+                        <li><a href="tabela.php?ntb=f3df806dc323278b7ceee93ff594327d">Partida</a></li>
+                        <li><a href="tabela.php?ntb=05b7ea9032cccacb4ecfa3785f42810c">Pessoa</a></li>
+                        <li><a href="tabela.php?ntb=be5d5600f69779cad9d135cb9b83af79">Vendeu</a></li>
                     </ul>
                     <!-- /.nav-second-level -->
                 </li>
-                <li><a href="forms.html"><i class="fa fa-edit fa-fw"></i> Relatórios</a></li>
+                <li><a href="#"><i class="fa fa-edit fa-fw"></i> Relatórios</a></li>
             </ul>
         </div>
         <!-- /.sidebar-collapse -->
