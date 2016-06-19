ceb78b901ca4355b019d5b88f289480c4a88d823
theghostcitizen <theomendes@aluno.puc-rio.br>
2016-06-19 19:33:11 -0300

    FIX FINAL

--- Trab2/10_PHP/relatorios.php
+++ Trab2/10_PHP/relatorios.php
@@ -176,24 +176,7 @@ $pdo = BancodeDados::conecta();
     <script src="js/bootstrap.min.js"></script>
     <!-- Metis Menu Plugin JavaScript -->
     <script src="js/metisMenu.min.js"></script>
-    <!-- DataTables JavaScript -->
-    <script src="js/jquery.dataTables.min.js"></script>
-    <script src="js/dataTables.bootstrap.min.js"></script>
     <!-- Custom Theme JavaScript -->
     <script src="js/sb-admin-2.js"></script>
-    <script>
-    $(document).ready(function() {
-        $('#MainTabela').DataTable({
-                responsive: true
-        });
-    });
-    </script>
-    <script>
-    $(document).ready(function() {
-        $('#SecTabela').DataTable({
-                responsive: true
-        });
-    });
-    </script>
 </body>
 </html>
